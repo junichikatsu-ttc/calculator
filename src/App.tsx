abab5bd5ba9379b748af216ad73b0db2ba27b733
junichikatsu-ttc <jk66302@ga.ttc.ac.jp>
2023-07-01 19:01:59 +0900

    デザインに合わせたフレームを実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>電卓</p>
-      </header>
+    <div className="calc">
+      <header>電卓</header>
+      <div className="display">
+        {/* 計算結果の表示を実装する */}
+      </div>
+      <div className="input">
+        <div className="numbers">
+          {/* 数字のボタンや「.」、「=」ボタン */}
+        </div>
+        <div className="operators">
+          {/* 「+」等のボタンを実装する */}
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
